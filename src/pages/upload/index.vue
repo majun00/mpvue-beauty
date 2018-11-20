742352dd08bbadfac53eef13ddb06f0da7312f0f
majun <majun@bmkp.cn>
2018-11-20 10:19:05 +0800

    feat:完成列表&预览下载

---
+++ src/pages/upload/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    我要上传
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
